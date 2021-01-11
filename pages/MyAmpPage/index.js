2fa06ab4839b854f72b2e90e8b67fc505ff2c952
katrpilar <kbrinso@gmail.com>
2021-01-11 10:15:39 -0800

    testing params as vars instead of extraUrlParams

--- pages/MyAmpPage/index.js
+++ pages/MyAmpPage/index.js
@@ -97,34 +97,32 @@ function MyAmpPage() {
               "event_name": "view_item_list",
               "event_category": "engagement",
               "value": "0",
-              "method": "Google"
-            },
-        "extraUrlParams": {
-          "items": [
-            {
-              "id": "P12345",
-              "name": "Android Warhol T-Shirt",
-              "list_name": "Search Results",
-              "brand": "Google",
-              "category": "Apparel/T-Shirts",
-              "variant": "Black",
-              "list_position": 1,
-              "quantity": 2,
-              "price": 2
-            },
-            {
-              "id": "P67890",
-              "name": "Flame challenge TShirt",
-              "list_name": "Search Results",
-              "brand": "MyBrand",
-              "category": "Apparel/T-Shirts",
-              "variant": "Red",
-              "list_position": 2,
-              "quantity": 1,
-              "price": 3
+              "method": "Google",
+              "items": [
+                {
+                  "id": "P12345",
+                  "name": "Android Warhol T-Shirt",
+                  "list_name": "Search Results",
+                  "brand": "Google",
+                  "category": "Apparel/T-Shirts",
+                  "variant": "Black",
+                  "list_position": 1,
+                  "quantity": 2,
+                  "price": 2
+                },
+                {
+                  "id": "P67890",
+                  "name": "Flame challenge TShirt",
+                  "list_name": "Search Results",
+                  "brand": "MyBrand",
+                  "category": "Apparel/T-Shirts",
+                  "variant": "Red",
+                  "list_position": 2,
+                  "quantity": 1,
+                  "price": 3
+                }
+              ]
             }
-          ]
-        }
       }
     }
   };
